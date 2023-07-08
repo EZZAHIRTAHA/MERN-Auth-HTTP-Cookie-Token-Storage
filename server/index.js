1e6a6bf2b460214be03cb669577f3e11bf43ac5d
Ezzahir Taha <taha1.ezzahir@gmail.com>
2023-07-09 01:08:27 +0200

    Implement user authentication and API endpoints for backend

--- server/index.js
+++ server/index.js
@@ -1,12 +1,19 @@
-const express = require('express');
+import express from 'express'
+import colors from 'colors'
+import dotenv from 'dotenv';
+
+
+dotenv.config()
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000
 
 
 app.listen(PORT, () => {
-    console.log(`App listening on http://localhost:${PORT}`);
+    console.log(`App listening on http://localhost:${PORT}`.cyan.underline.bold);
 })
 
 app.get('/', (req, res) => {
     res.send('Hello! ')
 })
+
+
