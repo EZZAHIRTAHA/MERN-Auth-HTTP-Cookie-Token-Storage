c0fa6dd74d12cb4818a39567a980b13b79ebee68
Ezzahir Taha <taha1.ezzahir@gmail.com>
2023-07-27 19:51:50 +0200

    Feat: Add responsive navigation bar

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,41 @@
+import  { useState } from 'react'
+import {FiLogIn} from 'react-icons/fi'
+import {AiOutlineMenu} from 'react-icons/ai'
+
+
+const Navbar = () => {
+
+    const [toggle, setToggle] = useState(false)
+    const toggleBar = () => {
+        
+    }
+    const MediaLinks = () => {
+        return(
+            <nav className='w-full flex flex-col justify-center items-center'>
+                
+            </nav>
+        )
+    }
+
+  return (
+            <nav class="bg-white border-gray-200 dark:bg-gray-900">
+                <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
+                    <a href="/" class="flex items-center">
+                        <h1 className='text-2xl text-white tracking-wider font-semibold'>Mern Auth</h1>
+                        
+                    </a>
+                    <div class="flex capitalize items-center justify-center ">
+                        <div className="sm:flex hidden">
+                            <a href="#" class="mr-6 text-md font-medium  flex justify-center items-center gap-2 text-white hover:underline "><FiLogIn/>Sign up</a>
+                            <a href="#" class="text-md hover:underline flex justify-center items-center gap-2 font-medium text-white"><FiLogIn/>Sign in </a>
+                        </div>
+                        <div className="sm:hidden flex">
+                            <AiOutlineMenu className='text-white text-4xl'/>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+  )
+}
+
+export default Navbar
